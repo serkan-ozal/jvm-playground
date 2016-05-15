f014ad42946bb9fded698e03767cdb6e1e59cead
Serkan ÖZAL <serkan@hazelcast.com>
2016-05-15 23:43:36 +0300

    comment typo fix in `WhiteBoxDemo`

--- src/main/java/tr/com/serkanozal/jvm/playground/WhiteBoxDemo.java
+++ src/main/java/tr/com/serkanozal/jvm/playground/WhiteBoxDemo.java
@@ -21,8 +21,8 @@ public class WhiteBoxDemo {
         Object obj = new Object();
         System.out.println("Is Object in old gen: " + wb.isObjectInOldGen(obj));
         
-        // Age bit is represented with 4 bit, so it can be 15 at most.
-        // This means that an object can live in young generation during 15 minor GC.
+        // Object age is represented with 4 bits, so it can be 15 at most.
+        // This means that an object can live in young generation during 15 minor GC at most.
         // So 16 minor GC guarantees that object is promoted to old generation.
         for (int i = 0; i < 16; i++) {
             System.out.println("Triggering young gc for " + (i + 1) + ". time ...");
